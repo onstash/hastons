f27a979986af434c2d6a3f9b5143aa531a892511
Santosh Venkatraman <santoshvenkatraman@Santoshs-Laptop.local>
2024-08-10 20:14:24 +0530

    Add tools/how-to posts on YouTube download

---
+++ pages/blog/tools/how-to/download-youtube-shorts.tsx
@@ -0,0 +1,10 @@
+import MDXLayout from "@/blog/components/layout";
+import HowToDownloadYouTubeShorts from "@/blog/markdown/tools/how-to-download-youtube-shorts.mdx";
+
+export default function Page() {
+  return (
+    <MDXLayout>
+      <HowToDownloadYouTubeShorts />
+    </MDXLayout>
+  );
+}
---
+++ pages/blog/tools/how-to/download-youtube-videos.tsx
@@ -0,0 +1,10 @@
+import MDXLayout from "@/blog/components/layout";
+import HowToDownloadYouTubeVideos from "@/blog/markdown/tools/how-to-download-youtube-videos.mdx";
+
+export default function Page() {
+  return (
+    <MDXLayout>
+      <HowToDownloadYouTubeVideos />
+    </MDXLayout>
+  );
+}
