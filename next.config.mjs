00aef4a70614ac3e399602a85ac68d5cc28ee1c5
Santosh Venkatraman <santoshvenkatraman@Santoshs-Laptop.local>
2024-08-10 20:01:36 +0530

    Remove frontmatter logic

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import remarkGfm from 'remark-gfm'
 import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
@@ -27,10 +26,6 @@ const nextConfig = {
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
-  },
 })
 
 // Merge MDX config with Next.js config
