dabf8bb706cca17824fc26bc3419ff0114129769
Santosh Venkatraman <santoshvenkatraman@Santoshs-Laptop.local>
2024-08-10 19:04:44 +0530

    Get basic blog working

---
+++ blog/components/layout.tsx
@@ -0,0 +1,16 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const pageStyle = {
+  maxWidth: 768,
+  marginLeft: "auto",
+  marginRight: "auto",
+  backgroundColor: "#FFFFFF",
+  ...inter.style,
+};
+
+export default function MDXLayout({ children }: { children: React.ReactNode }) {
+  // Create any shared layout or styles here
+  return <section style={pageStyle}>{children}</section>;
+}
---
+++ pages/blog/index.tsx
@@ -0,0 +1,10 @@
+import MDXLayout from "@/blog/components/layout";
+import Welcome from "@/blog/markdown/posts/welcome.mdx";
+
+export default function Page() {
+  return (
+    <MDXLayout>
+      <Welcome />
+    </MDXLayout>
+  );
+}
