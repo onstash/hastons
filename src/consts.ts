0fc8c6fcd2a941e4fac729095e4c3f3ce41fcfe4
Santosh Venkatraman <santoshvenkatraman@Santoshs-Laptop.local>
2025-12-24 21:13:07 +0530

    feat: update consts file

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
+export const SITE_URL = "https://hastons.vercel.app";
+export const SITE_TITLE = "Hastons";
 export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
+  "Hastons is a personal blog built with Astro and Tailwind CSS.";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "santosh.venk+hastonsvercel@gmail.com";
